2b686645c184114f0bce5128ac0dbf73e8cd69d7
Matt Gallucci <mattgallucci97@gmail.com>
2022-10-31 19:01:54 -0700

    implemented basic router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,22 @@
 import 'antd/dist/antd.css';
-import Register from './components/Register';
 import { Layout } from 'antd';
-import { FireOutlined } from '@ant-design/icons';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
 import Login from './components/Login';
+import Splash from './components/Splash';
+import BaseLayout from './components/BaseLayout';
 
 const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <Layout style={{ width: '100%', height: '100%' }}>
-        <Header></Header>
-        <Content style={{ padding: '50px', width: '100%', justifyContent: 'center' }}>
-          <Login />
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>SweatshopCalabasas ©2022</Footer>
-      </Layout>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<BaseLayout childComp={Login} />} />
+          <Route path="/register" element={<BaseLayout childComp={Register} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
