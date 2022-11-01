1771a62cbeda7ec26f058be722da89a9f27e77bb
Matt Gallucci <mattgallucci97@gmail.com>
2022-10-31 18:16:40 -0700

    updates to app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,48 +3,64 @@ import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import 'reflect-metadata';
 // routes
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import authRouter from './routes/auth.routes';
+import { AppDataSource } from './database/dataSource';
+import cors from 'cors';
+import AppError from './utils/AppError';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// view engine setup
-app.set('view engine', 'jade');
+// connect to DB
+AppDataSource.initialize().then(async () => {
+  const PORT = process.env.PORT || 3001;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+  // view engine setup
+  app.set('view engine', 'jade');
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// render react build
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+  app.use(logger('dev'));
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, '../public')));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    next();
+  });
 
-// catch 404 and forward to error handler
-app.use((req: Request, res: Response, next: NextFunction) => {
-  next(createError(404));
-});
+  // app.use('/', indexRouter);
+  // app.use('/users', usersRouter);
+  // render react build
+  app.use('/api/auth', authRouter);
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
-// error handler
-app.use((err: any, req: Request, res: Response) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // catch 404 and forward to error handler
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    next(createError(404));
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // error handler
+  app.use((err: AppError, req: Request, res: Response) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}...`);
+    // render the error page
+    res.status(err.statusCode || 500).json({
+      status: err.status,
+      message: err.message,
+    });
+    res.render('error');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}...`);
+  });
 });
 
 module.exports = app;
