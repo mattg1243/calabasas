2fe6b55474e24d24088d1d4efe3178cd9df9d648
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-02 00:27:10 -0700

    finished register page ui

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,30 +1,36 @@
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input, Alert, message } from 'antd';
 import React, { useState } from 'react';
 import axios from 'axios';
+import styles from '../index.module.css';
+import { Content } from 'antd/lib/layout/layout';
+import logo from '../assets/logo_four_squares.png';
+import type { AlertStateObj } from './Login';
+import { match } from 'assert';
 
 export default function Register(): JSX.Element {
   const [email, setEmail] = useState<string>('');
   const [username, setUsername] = useState<string>('');
-  const [fname, setFname] = useState<string>('');
-  const [lname, setLname] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [passwordConfirm, setConfirmPassword] = useState<string>('');
+  const [alert, setAlert] = useState<AlertStateObj | null>(null);
 
   const sendRegisterRequest = async () => {
     if (password === passwordConfirm) {
       try {
         const response = await axios.post('http://localhost:3001/api/auth/register', {
-          fname,
-          lname,
           email,
           username,
           password,
           acctType: 'artist',
         });
         console.log(response);
+        setAlert({ status: 'success', message: 'Account created succesfully, you may now login' });
       } catch (err) {
         console.error(err);
       }
+    } else {
+      // passwords dont match, display error
+      setAlert({ status: 'error', message: `Passwords don't match, please try again.` });
     }
   };
 
@@ -37,69 +43,90 @@ export default function Register(): JSX.Element {
   };
 
   return (
-    <Form
-      name="basic"
-      layout="vertical"
-      initialValues={{ remember: true }}
-      wrapperCol={{ span: 16, offset: 4 }}
-      labelCol={{ span: 16, offset: 4 }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-      style={{ justifyContent: 'center', textAlign: 'center', width: '100%' }}
-    >
-      <Form.Item
-        style={{ justifySelf: 'center' }}
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Please input your username!' }]}
+    <Content style={{ justifyContent: 'center', textAlign: 'center', marginTop: '5rem' }}>
+      <img src={logo} alt="logo" width={200} style={{ marginBottom: '1rem' }} />
+      <h1 style={{ fontSize: '3.5rem' }}>Create your free account</h1>
+      <Form
+        name="basic"
+        layout="vertical"
+        initialValues={{ remember: true }}
+        wrapperCol={{ span: 16, offset: 4 }}
+        labelCol={{ span: 16, offset: 4 }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        style={{ justifyContent: 'center', textAlign: 'center', width: '100%' }}
       >
-        <Input onChange={(e) => setEmail(e.target.value)} />
-      </Form.Item>
-
-      <Form.Item
-        style={{ justifySelf: 'center' }}
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input onChange={(e) => setUsername(e.target.value)} />
-      </Form.Item>
-
-      <Form.Item label="First Name">
-        <Input onChange={(e) => setFname(e.target.value)} />
-      </Form.Item>
-
-      <Form.Item label="Last Name">
-        <Input onChange={(e) => setLname(e.target.value)} />
-      </Form.Item>
-
-      <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-        <Input.Password onChange={(e) => setPassword(e.target.value)} />
-      </Form.Item>
-
-      <Form.Item
-        label="Confirm Password"
-        name="confirm password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password onChange={(e) => setConfirmPassword(e.target.value)} />
-      </Form.Item>
-
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 4, span: 16 }}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
-        <Button
-          type="primary"
-          onClick={async () => {
-            await sendRegisterRequest();
-          }}
+        <Form.Item
+          style={{ justifySelf: 'center' }}
+          name="email"
+          rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Input
+            className="round-white-input"
+            placeholder="Email"
+            style={{ width: '600px', height: '50px', borderRadius: '40px' }}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          style={{ justifySelf: 'center' }}
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input
+            className="round-white-input"
+            placeholder="Username"
+            style={{ width: '600px', height: '50px', borderRadius: '40px' }}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Item>
+
+        <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+          <Input.Password
+            className="round-white-input"
+            placeholder="Password"
+            style={{ width: '600px', height: '50px', borderRadius: '40px' }}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Item>
+
+        <Form.Item name="confirm password" rules={[{ required: true, message: 'Please input your password!' }]}>
+          <Input.Password
+            className="round-white-input"
+            placeholder="Confirm Password"
+            style={{ width: '600px', height: '50px', borderRadius: '40px' }}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
+          <Button
+            type="primary"
+            shape="round"
+            size="large"
+            style={{
+              fontSize: '2rem',
+              margin: '1rem',
+              background: 'black',
+              borderColor: 'black',
+              width: '400px',
+              height: '80px',
+            }}
+            onClick={async () => {
+              await sendRegisterRequest();
+            }}
+          >
+            Sign Up
+          </Button>
+          <h3>Already have an account?</h3>
+          <a href="/login">Login</a>
+        </Form.Item>
+      </Form>
+      {alert ? (
+        <Alert message={alert.message} type={alert.status} showIcon style={{ width: '50%', margin: 'auto' }} />
+      ) : null}
+    </Content>
   );
 }
