cbd301ed6f4e48641ea3cb8c2f3648160cc14477
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-05 09:44:32 -0700

    files are now organized by path prefix while being uploaded to bucket

--- src/bucket/audio.ts
+++ src/bucket/audio.ts
@@ -1,5 +1,6 @@
 import S3 from 'aws-sdk/clients/s3';
 import fs from 'fs';
+import { determineFileType } from '../utils/fileHelpers';
 // initiate client
 const bucketName = process.env.S3_BUCKET_NAME;
 const region = process.env.S3_BUCKET_REGION;
@@ -14,19 +15,24 @@ const s3Client = new S3({
 
 // upload the beat to s3
 // this could be refactored to work with all file types by simply
-// accepting the prefix of the path corresponding to file type to 
+// accepting the prefix of the path corresponding to file type to
 // use for the bucket location path
 // ie: uploading beat, prefix arg = /beat
 export const uploadBeatToBucket = async (file: Express.Multer.File) => {
-  const fileStream = fs.createReadStream(file.path);
+  try {
+    const prefix = determineFileType(file);
+    const fileStream = fs.createReadStream(file.path);
 
-  const uploadParams: S3.PutObjectRequest = {
-    Bucket: bucketName ? bucketName : '',
-    Body: fileStream,
-    Key: file.filename,
-  };
-  const response = await s3Client.upload(uploadParams).promise();
-  return Promise.resolve(response);
+    const uploadParams: S3.PutObjectRequest = {
+      Bucket: bucketName ? bucketName : '',
+      Body: fileStream,
+      Key: prefix + file.filename,
+    };
+    const response = await s3Client.upload(uploadParams).promise();
+    return Promise.resolve(response);
+  } catch (err: any) {
+    console.error(err.message);
+  }
 };
 
 // download the beat from 3
